e976a9ce1497e4f0ded81451d839de122ef57d7d
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2021-06-23 19:56:43 -0500

    Update interrupinterna.c

--- interrupinterna.c
+++ interrupinterna.c
@@ -6,12 +6,12 @@ volatile bool abajo = true;
 //FUNCIONES
 void interrupt_TIMER0_Init(){
  cli(); //deshabilitar momentáneamente las interrupciones
- TCCR0B = 0b101;
- TIMSK0 = 1;
+ TCCR0B = 0b101; //preescalador 1024
+ TIMSK0 = 1; // habilitar interrupcion por desbordamiento
  sei(); //habilitar las interrupciones
- TCNT0 = 12; //250ms
+ TCNT0 = 12; //250ms // tiempo de la interrupcion
 }
-ISR(TIMER0_OVF_vect){ //INTERRUPCIONES
+ISR(TIMER0_OVF_vect){ //INTERRUPCIONES //se hace el desplazamiento
  if(abajo){
  PORTC = PORTC<<1;
  if(PORTC == 64) PORTC = 1;
@@ -29,7 +29,7 @@ int main()
  PORTD |= (1<<PD2); //Activar resistencia de PULLDOWN en PIN0 del puerto D
  interrupt_TIMER0_Init(); //250ms de retardo
  while (1){
- if((PIND&(1<<PIND2))==0){ //si está presionado entra al if
+ if((PIND&(1<<PIND2))==0){ //si está presionado entra al if 
 while((PIND&(1<<PIND2))==0){} //espera a que se suelte la botonera
 abajo = !abajo;
  }
