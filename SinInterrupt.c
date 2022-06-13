d16f8b2fd7d58f004019f0294dd1fd0a6bc9de0d
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-06-13 08:41:07 -0500

    Update SinInterrupt.c

--- SinInterrupt.c
+++ SinInterrupt.c
@@ -5,28 +5,30 @@
 static volatile bool abajo = true; // static variable global // volatile diferentes hilos o interrupciones + programa principal
 //FUNCIONES
 void isPress(){
- if((PIND&(1<<PIND2))==0){ //si está presionado entra al if
- while((PIND&(1<<PIND2))==0){} //espera a que se suelte la botonera
+ if(!(PIND&(1<<PIND2))){ //si está presionado entra al if
+ while(!(PIND&(1<<PIND2))){} //espera a que se suelte la botonera
  abajo = !abajo;
  }
 }
 //MAIN
 int main()
 {
- DDRC = 0XFF; //PuertoC salida
- PORTC = 1; //PIN0 del puerto C activado
+ DDRB = 0XFF; //PuertoC salida
+ PORTB = 1; //PIN0 del puerto C activado
  PORTD |= (1<<PD2); //Activar resistencia de PULLUP en PIN2 del puerto D
  while (1){
  _delay_ms(250);
  if(abajo){
-PORTC = PORTC<<1;
-if(PORTC == 64) PORTC = 1;
+PORTB = PORTB<<1;
+if(PORTB == 64) PORTB = 1;
  }
  else{
-PORTC = PORTC>>1;
-if(PORTC == 0) PORTC = 32;
+PORTB = PORTB>>1;
+if(PORTB == 0) PORTB = 32;
  }
  isPress();
  }
  return 0;
 }
+
+
