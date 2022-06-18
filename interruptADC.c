46f08b117aecea67849ce769c9b419bf3cf41e46
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-06-18 09:33:16 -0500

    Update interruptADC.c

--- interruptADC.c
+++ interruptADC.c
@@ -2,11 +2,14 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <stdbool.h>
+#include "uart.h"
+#include <stdlib.h>
+
 //FUNCIONES
 void interrupt_INT0_Init(){
  cli(); //DESH INT
  EICRA = 0b11; // INT FLANCO DE SUBIDA
- EIMSK = 1; // HAB PIN0
+ EIMSK = 1; // HAB PIN0, para habilitar pin2 ---> 2
  sei(); //HAB INT
 }
 void interrupt_TIMER0_Init(){
@@ -17,7 +20,7 @@ void interrupt_TIMER0_Init(){
  TCNT0 = 12; //250ms
 }
 void ADC_Init(){
- ADMUX = 0b110; //ADC6 (ADC6 Y ADC7 son solo analógicos entonces no es necesario configurar el reg DIDR0)
+ ADMUX = 0b000; //ADC0 
  ADCSRA = 0b10101011; //ADC activado modo automático y preescalador 8
  //ADCSRB= 0b10; //Modo interrupción externa //ADIE(3) en 1 para habilitar la interrupción por la terminación de una conversión analógica.
  ADCSRB= 0b100; //Modo Timer0 (250ms)
@@ -29,21 +32,20 @@ ISR(TIMER0_OVF_vect){
 ISR(INT0_vect){
  //EIFR &= ~(1<<INTF0);
 }
-ISR(ADC_vect){ //ENCERAR EL BIT 4 DEL REG ADCSRA &=~(1<<ADIF)
- PORTB =ADCL;
- PORTC = ADCH;
+ISR(ADC_vect){ 
+ //PORTB =ADCL;
+ //PORTC = ADCH;
+float r = ADC/1023.0*5.0;
+char rec1[20] ;
+dtostrf(r, 2, 4, rec1);
+serial_println_str(rec1);
 }
 //MAIN
-int main()
+void ctrl()
 {
- DDRB = 0XFF; //PuertoB salida
- DDRC = 0b11; //Pines0y 1 del puerto C como salida
  PORTD |= (1<<PD2); //Activar resistencia de PULLUP en PIN0 del puerto D
  ADC_Init();
  interrupt_INT0_Init();
  interrupt_TIMER0_Init(); //250ms de retardo
- while (1){
- //Lazo principal vacío
- }
- return 0;
+
 }
