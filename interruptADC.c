e9b6f9a14651faad7d3298dcdeb1e6de99496e22
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-10-30 20:20:36 -0500

    Update interruptADC.c

--- interruptADC.c
+++ interruptADC.c
@@ -2,7 +2,7 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <stdbool.h>
-#include "uart.h"
+
 #include <stdlib.h>
 
 //FUNCIONES
@@ -12,42 +12,55 @@ void interrupt_INT0_Init(){
  EIMSK = 1; // HAB PIN0, para habilitar pin2 ---> 2
  sei(); //HAB INT
 }
-void interrupt_TIMER0_Init(){
+void interrupt_TIMER1_Init(){
  cli();
- TCCR0B = 0b101; //PREESCALADOR 1024
- TIMSK0 = 1; //HB TIMER POR DESBORDAMIENTO
+ TCCR1A=0;
+ TCCR1B = 0b101; //PREESCALADOR 1024
+ TIMSK1 = 1; //HB TIMER POR DESBORDAMIENTO
  sei();
- TCNT0 = 12; //250ms
+ TCNT1 =  61630; //250ms   para 1 segundo 49911
 }
 void ADC_Init(){
  ADMUX = 0b000; //ADC0 
- ADCSRA = 0b10101011; //ADC activado modo automático y preescalador 8
+ ADCSRA = 0b10101111; //ADC activado modo automático y preescalador 8
  //ADCSRB= 0b10; //Modo interrupción externa //ADIE(3) en 1 para habilitar la interrupción por la terminación de una conversión analógica.
- ADCSRB= 0b100; //Modo Timer0 (250ms)
+ ADCSRB= 0b110; //Modo Timer1 (250ms)
 }
 //INTERRUPCIONES
-ISR(TIMER0_OVF_vect){
- TCNT0 = 12;
+ISR(TIMER1_OVF_vect){
+ TCNT1 =  61630;
 }
 ISR(INT0_vect){
  //EIFR &= ~(1<<INTF0);
 }
 ISR(ADC_vect){ 
- //PORTB =ADCL;
- //PORTC = ADCH;
+
 float r = ADC/1023.0*5.0;
-char rec1[20] ;
-dtostrf(r, 2, 4, rec1);
-serial_println_str(rec1);
+
+	 if (r>=0.49 && r<1.00) {PORTB=1; PORTD &= ~0B11110000; }
+else if (r>=1.00 && r<1.50) {PORTB=3;  PORTD = 0;  }
+else if (r>=1.50 && r<2.00) {PORTB=7;  PORTD = 0;  }
+else if (r>=2.00 && r<2.50) {PORTB=15; PORTD = 0;  }
+else if (r>=2.50 && r<3.00) {PORTB=31; PORTD = 0;  }
+else if (r>=3.00 && r<3.50) {PORTB=63; PORTD = 0;  }
+//else if (r>=3.50) {PORTB=63; PORTD =  0X10 ;  }	//PARA USAR ESTO, SE REQUIERE REAZILAR LA CONEXION DE PULL UP EXTERNAMENTE
+else if (r>=3.50 && r<4.00) {PORTB=63; PORTD |=  0B00010000; PORTD &= ~(1<<5) ;  }
+else if (r>=4.00 && r<4.50) {PORTB=63; PORTD |=  0B00110000; PORTD &= ~(1<<6) ;  }
+else if (r>=4.50 && r<5.00) {PORTB=63; PORTD |=  0B01110000; PORTD &= ~(1<<7) ;  }
+else if (r>=5.00) {PORTB=63; PORTD |=  0B11110000;  }
+else 	{PORTB = 0; PORTD = 0;}
+
 }
 //MAIN
 int main()
 {
+ DDRB = 0XFF;
+ DDRD |= 0B11110000;
  PORTD |= (1<<PD2); //Activar resistencia de PULLUP en PIN0 del puerto D
  ADC_Init();
  interrupt_INT0_Init();
- interrupt_TIMER0_Init(); //250ms de retardo
-serial_begin(9600);
+ interrupt_TIMER1_Init(); //250ms de retardo
+//serial_begin(9600);
 while(1){
 ;
 }
